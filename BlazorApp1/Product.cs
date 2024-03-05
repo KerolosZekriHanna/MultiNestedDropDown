975abb40161b9fed48f4b0cfaa7cf6504ed028ff
kerolos.zekri <kerolos.zekri.hanna@gmail.com>
2024-03-05 21:39:14 +0200

    add project

---
+++ BlazorApp1/Product.cs
@@ -0,0 +1,25 @@
+﻿namespace BlazorApp1
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public int BusinessCapabilityLevel { get; set; }
+        public string BusinessCapabilittyName { get; set; }
+
+        public string Prefix
+        {
+            get
+            {
+                return BusinessCapabilittyName?.Split(' ').FirstOrDefault()?.TrimEnd('.'); ;
+            }
+        }
+
+       
+
+    }
+
+    public enum CapabilityLevelEnum
+    {
+        Level_1 = 1, Level_2 = 2, Level_3 = 3
+    }
+}
---
+++ BlazorApp1/Vector.sql
@@ -0,0 +1,23 @@
+﻿CREATE PROCEDURE FindTop5SimilarVectors
+AS
+BEGIN
+    DECLARE @Vector1 NVARCHAR(MAX)
+    DECLARE @VectorId INT
+    DECLARE @Vectors TABLE (Id INT, Vector NVARCHAR(MAX))
+
+    -- Get the vector with Id=1
+    SELECT @Vector1 = Vector
+    FROM Vectors
+    WHERE Id = 1
+
+    -- Insert all vectors into a temporary table
+    INSERT INTO @Vectors (Id, Vector)
+    SELECT Id, Vector
+    FROM Vectors
+    WHERE Id != 1
+
+    -- Calculate cosine similarity and select top 5 similar vectors
+    SELECT TOP 5 Id
+    FROM @Vectors
+    ORDER BY dbo.CosineSimilarity(@Vector1, Vector) DESC
+END
